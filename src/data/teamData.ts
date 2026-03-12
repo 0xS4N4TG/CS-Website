0beb054f5f9d7f9428f3f1e08832b4695e247c07
Tanmoy mandal <tanmaymandal1250@gmail.com>
2026-03-13 03:04:53 +0530

    EC data uploaded

--- src/data/teamData.ts
+++ src/data/teamData.ts
@@ -8,35 +8,62 @@ export interface TeamMember {
 }
 
 export const EC_MEMBERS: TeamMember[] = [
-  { 
-    name: 'Arya Sharma',  
-    role: 'President',      
-    image: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&h=500&fit=crop&q=80',
-    linkedin: '#',
-    github: '#',
-    instagram: '#'
+    {
+    name: "Samaksh Gupta",
+    image: "https://images.prismic.io/ieeemuj/ZnJjw5m069VX13S1_IMG-20240405-WA0024~2-SamakshGupta.jpg?auto=format,compress",
+    role: "Chairperson",
+    linkedin: "https://www.linkedin.com/in/samakshgupta04",
+    github: "https://github.com/AwesomeSam9523/",
+    instagram: "https://www.instagram.com/samaksh.gupta04?igsh=c252dTcwNjZldWRp",
   },
-  { 
-    name: 'Rohan Verma',  
-    role: 'Vice President', 
-    image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=400&h=500&fit=crop&q=80',
-    linkedin: '#',
-    instagram: '#' 
-  },
-  { 
-    name: 'Priya Nair',   
-    role: 'Secretary',      
-    image: 'https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?w=400&h=500&fit=crop&q=80',
-    linkedin: '#',
-    instagram: '#' 
-  },
-  { 
-    name: 'Kabir Singh',  
-    role: 'Treasurer',      
-    image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=500&fit=crop&q=80',
-    linkedin: '#',
-    instagram: '#' 
-  },
+ {
+  name: "Tamanna Yadav",
+  image: "https://images.prismic.io/ieeemuj/ZnHEBZm069VX12wA_IMG_20240617_153104-TamannaYadav.jpg?auto=format,compress",
+  role: "Vice Chairperson",
+  linkedin: "https://www.linkedin.com/in/tamanna-yadav-4251a6234",
+  github: "",
+  instagram: "https://www.instagram.com/tamanna.yadav_?igsh=MWl3Ym16bDMyOHBtZg==",
+},
+{
+  name: "Salaj Singh Bisht",
+  image: "https://images.prismic.io/ieeemuj/ZnHLL5m069VX12yn_IMG_20240416_161408-SalajBisht.jpg?auto=format,compress",
+  role: "General Secretary",
+  linkedin: "https://www.linkedin.com/in/salaj-singh-bisht-837899248",
+  github: "",
+  instagram: "https://instagram.com/_.bishttt._",
+},
+{
+  name: "Aryan Verma",
+  image: "https://images.prismic.io/ieeemuj/aENAFrh8WN-LVx3f_IMG_0788-AryanVerma.jpeg?auto=format,compress",
+  role: "Managing Director",
+  linkedin: "https://www.linkedin.com/in/aryannnnverma",
+  github: "https://github.com/aryannverse",
+  instagram: "https://www.instagram.com/aryannverse",
+},
+{
+  name: "Vinayak Jajoo",
+  image: "https://images.prismic.io/ieeemuj/aENAfrh8WN-LVx4C_IMG_6284-VinayakJajoo.heic?auto=format,compress",
+  role: "Treasurer",
+  linkedin: "https://www.linkedin.com/in/vinayak-jajoo-414823250?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+  github: "",
+  instagram: "https://www.instagram.com/vinayakjajoo?igsh=MWZ3NGFkNm83OTEwYg%3D%3D&utm_source=qr",
+},
+{
+  name: "Bhavya Jaggi",
+  image: "https://images.prismic.io/ieeemuj/aEM_tbh8WN-LVx3E_20250421_183325-BhavyaJaggi.jpg?auto=format,compress",
+  role: "Human Resource Director",
+  linkedin: "https://www.linkedin.com/in/bhavya-jaggi-20b768284?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  github: "https://github.com/BhavyaJaggi12",
+  instagram: "https://www.instagram.com/bhav.ya_jaggi?igsh=MTQ0dGJjYWNjeXg0ag==",
+},
+{
+  name: "Neil Gupta",
+  image: "https://images.prismic.io/ieeemuj/aENAUbh8WN-LVx31_IMG_2745-NeilGupta.jpeg?auto=format,compress",
+  role: "Technical Secretary",
+  linkedin: "https://www.linkedin.com/in/neil-gupta-97a0242a1",
+  github: "",
+  instagram: "https://www.instagram.com/neilguptaa._?igsh=MWF0cXV5bHZ1Z3BsMg==",
+},
 ];
 
 export const WEB_MEMBERS: TeamMember[] = [
