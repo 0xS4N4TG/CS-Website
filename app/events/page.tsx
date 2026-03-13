d2a65e2caec30f74b2f4d9ed9f204e4c85527aef
HarshvardhanKh <harshvardhan.khaitan1202@gmail.com>
2026-03-13 16:44:17 +0530

    working

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,24 +1,37 @@
 "use client";
 
-import React from 'react'
-import LineBackground from "@/components/LineBackground";
+import React from "react";
 import TiltedPhotoGrid from "@/src/components/common/TiltedPhotoGrid";
-import Eventcard from "@/src/components/common/Eventcard"
+import Eventcard from "@/src/components/common/Eventcard";
+
+function TornEdge() {
+  return (
+    <div className="absolute bottom-0 left-0 w-full z-20 pointer-events-none">
+      <img
+        src="/images/events/tear.svg"
+        alt="tear divider"
+        className="w-full translate-y-1/4 opacity-100"
+      />
+    </div>
+  );
+}
 
 export default function Event() {
-    return (
-        <>
-        <div className="relative min-h-screen bg-gradient-to-b from-gray-500 to-black overflow-hidden">
-          
+  return (
+    <>
+      <section className="relative min-h-screen bg-gradient-to-b from-gray-500 to-black overflow-hidden">
 
-            <div className="absolute inset-0 z-10 overflow-hidden pointer-events-none">
-                <TiltedPhotoGrid />
-            </div>
-
-            
-            
+        <div className="absolute inset-0 z-0">
+          <TiltedPhotoGrid />
         </div>
-        <div className="overflow-hidden"><Eventcard/></div>
-        </>
-    )
+
+        <TornEdge />
+
+      </section>
+
+      <section className="relative bg-black overflow-hidden">
+        <Eventcard />
+      </section>
+    </>
+  );
 }
